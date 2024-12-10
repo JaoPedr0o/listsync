2597f2944f882d4034136d50adc48143f0ded2b9
JaoPedr0o <joao.pedro@b2list.com>
2024-12-10 17:25:19 -0300

    first commit

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,5 +1,19 @@
-import { ScreenContent } from 'components/ScreenContent';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
-  return <ScreenContent path="screens/home.tsx" title="Home" />;
+  return (
+    <View style={styles.mainContainer}>
+      <Text>Inicio</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#FFFFFF',
+    height: '100%',
+  },
+});
